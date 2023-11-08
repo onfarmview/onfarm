32c335357448735ce9bcff324c5036b8b53b2d16
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-11-09 11:20:14 +1300

    add index

--- web.py
+++ web.py
@@ -2,19 +2,28 @@ import folium
 import pandas
 import geemap.foliumap as geemap
 map_center=(-43.525650, 172.639847)
-map = geemap.Map(
+Map = geemap.Map(
     basemap="HYBRID",
     plugin_Draw=True,
     Draw_export=True,
     locate_control=True,
     plugin_LatLngPopup=False, center=map_center, zoom=6.25,
 )
-popup_message = 'Contact: admin@onfarmview.com'
-folium.Marker(
-    location=map_center,
-    popup=popup_message,
-    icon=folium.Icon(color='blue')
-).add_to(map)
 
-map.addLayerControl()
-map.save("index.html")
+bounds = Map.get_bounds()
+
+popup_message = 'Contact: admin@onfarmview.com'+ str(bounds)
+# folium.Marker(
+#     location=map_center,
+#     popup=popup_message,
+#     icon=folium.Icon(color='blue')
+# ).add_to(map)
+marker = folium.Marker(
+    location=map_center, #[bounds[0][2], bounds[1][3]],  # Use the top-right corner coordinates of the map
+    popup=popup_message,
+    icon=folium.Icon(icon="info-sign", color="red")
+)
+Map.add_child(marker)
+
+Map.addLayerControl()
+Map.save("index.html")
