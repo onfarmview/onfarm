ca5fca75f4ed78f089366c1b86c9f3420574141b
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-11-08 16:29:08 +1300

    Initial commit

--- map_script.py
+++ map_script.py
@@ -1,7 +1,32 @@
-#!pip install folium
 import folium
+import pandas
 
-map_1 = folium.Map(location=[-22.951916, -43.210466], zoom_start=15)
-folium.CircleMarker(location=[-22.951916, -43.210466], radius=15, popup="Christ the Redeemer").add_to(map_1)
-folium.Marker(location=[-22.951916, -43.210466], popup="Christ the Redeemer").add_to(map_1)
-map_1.save("index.html")
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation) :
+    if elevation < 1500 :
+        return "green"
+    elif elevation < 3000 :
+        return "orange"
+    else :
+        return "red"
+
+map = folium.Map(location = [38.58, -99.89], zoom_start = 6, tiles = "Mapbox Bright")
+
+fgv = folium.FeatureGroup(name = "Volcanoes_USA")
+
+for lt, ln, el in zip(lat, lon, elev) :
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 6 , popup = folium.Popup(str(el) + " m", parse_html = True), fill_color = color_producer(el), color = "grey", fill_opacity = 0.7, fill = True))
+
+fgp = folium.FeatureGroup(name = "Population")
+
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(), style_function = lambda x : {'fillColor' : 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("index.html")
