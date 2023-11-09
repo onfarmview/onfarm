f85615daaea23f83bad785e8fdc4a875970bbad8
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-11-10 10:35:35 +1300

    Top menu

--- web.py
+++ web.py
@@ -17,7 +17,18 @@ def maskCloudAndShadows(image):
   mask = (cloud.And(snow)).And(cirrus.neq(1)).And(shadow.neq(1))
   return image.updateMask(mask).divide(10000)
 
-ee_authenticate(token_name="4/1AfJohXkTlWMKd8fPevD3hd4tAq_j-YlD2CabTy7QtM7iu1gNB3XdBEqRehA")
+# Normalized difference vegetation index (NDVI)
+def getNDVI(image): 
+    ndvi = image.normalizedDifference(['B8','B4']).rename("NDVI")
+    image = image.addBands(ndvi)
+    return(image)
+
+def addDate(image):
+    img_date = ee.Date(image.date())
+    img_date = ee.Number.parse(img_date.format('YYYYMMdd'))
+    return image.addBands(ee.Image(img_date).rename('date').toInt())
+
+ee_authenticate(token_name="4/1AfJohXkTlWMKd8fPevD3hd4tAq_j-YlD2CabTy7QtM7iu1gNB3XdBEqRehA")  #4/1AfJohXkTlWMKd8fPevD3hd4tAq_j-YlD2CabTy7QtM7iu1gNB3XdBEqRehA
 
 map_center=(-43.525650, 172.639847)
 popup_message = 'Contact: admin@onfarmview.com'
@@ -45,6 +56,15 @@ se2 = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(
             startDate,endDate).filter(
             ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",80)).map(maskCloudAndShadows).median()
 
+palette = ['FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901', '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01', '012E01', '011D01', '011301']
+vis_params = {
+  'min': 0,
+  'max': 1,
+  'palette': palette}
+
+NDVI_data = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(startDate, endDate).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",80)).map(maskCloudAndShadows).map(getNDVI)
+Map.addLayer(NDVI_data.select('NDVI').median(), vis_params, "Median of NDVI")        
+
 
 band = ['B4','B3','B2']
 rgbViza = {"min":0.0, "max":0.7,"bands":band}
@@ -71,6 +91,7 @@ band = ['B8','B11','B2']
 rgbViza = {"min":0.0, "max":0.7,"bands":band}
 titlemap = "Sentinel 2 - Healthy Vegetation"
 Map.addLayer(se2, rgbViza, titlemap)
+
 
 
 folium.Marker(
